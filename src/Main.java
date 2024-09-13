2374c655cfe008ef600c40eff44eaf66ca560f7a
robertMac <poppi.r@gmx.at>
2024-09-13 11:57:06 +0200

    improved Start messages

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,25 @@ import java.net.ServerSocket;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
-/*
-        ServerSocket server = new ServerSocket(2048);
-        var sockerPlayer1=server.accept();
-        var sockerPlayer2=server.accept();
 
+        ServerSocket server = new ServerSocket(2048);
+
+        var sockerPlayer1=server.accept();
         var is1=sockerPlayer1.getInputStream();
         var os1 = new PrintStream(sockerPlayer1.getOutputStream());
+        os1.println("Hello. You are Plyer ONE. Waiting for Player Two......");
+
+        var sockerPlayer2=server.accept();
         var is2=sockerPlayer2.getInputStream();
         var os2 = new PrintStream(sockerPlayer2.getOutputStream());
-*/
+        os2.println("Hello. You are Plyer TWO. Player ONE has already joined. Lets Go");
 
+/*
         var is1=System.in;
         var os1 = System.out;
         var is2=System.in;
         var os2 = System.out;
-
+*/
 
         Config config=new Config();
         Board board=new Board(config);
